7e73b7830fd829dc6f73094a8f552ccb405837a1
Lorenz <apps.lal@gmail.com>
2019-08-18 10:50:53 +0200

    Refactored setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from setuptools import setup
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(name='deepdream',
-      version='0.1',
+      version='0.1.0',
       description='Stylize images using the deep dream approach',
+      long_description=README,
+      long_description_content_type="text/markdown",
       author='Lorenz Lehmann',
       author_email='apps.lal@gmail.com',
-      packages=['deepdream'],
+      license="MIT",
+      packages=find_packages(exclude=("tests",)),
       zip_safe=False)
