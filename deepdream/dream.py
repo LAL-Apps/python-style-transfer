1f659ee992e246157b5aa2471bd2a2b43b387524
Lorenz <apps.lal@gmail.com>
2019-08-19 00:01:48 +0200

    Moved image prep tp model

--- deepdream/dream.py
+++ deepdream/dream.py
@@ -4,34 +4,9 @@ Module to apply a deep dream to style and content image
 import logging
 import requests
 import io
-import torch
-import numpy as np
-import torch.optim as optim
-from torchvision import transforms, models
 from PIL import Image
 
 from deepdream.exceptions import ImageLoadException
-
-def prepImage(image,maxSize=400):
-    '''
-    Helper function to prepare an image by scaling and transforming
-    :param image: PIL image object to scale
-    :param maxSize: Max size of the picture. Reduce for faster training
-    :return: Normalized image as tensor
-    '''
-    #Determine if image should be resized
-    size = maxSize if max(image.size) > maxSize else max(image.size)
-
-    #Apply the transformation to scale and normalize the image
-    transform = transforms.Compose([
-                        transforms.Resize(size),
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.485, 0.456, 0.406),
-                                             (0.229, 0.224, 0.225))])
-    image = transform(image)
-    #Remove the alpha dimension
-    image = image[:3,:,:] #TODO unsqueeze to support batch dimension?
-    return image
 
 def loadImageFromUrl(url):
     '''
@@ -67,8 +42,7 @@ class Dream():
         :param path: Path to the image file
         '''
         #Load image and convert to RGB. Standard is RBG
-        image = Image.open(path).convert('RGB')
-        self.contentImage = prepImage(image)
+        self.contentImage = Image.open(path).convert('RGB')
 
     def setContentFromUrl(self, url):
         '''
@@ -76,8 +50,7 @@ class Dream():
 
         :param url: URL to the image
         '''
-        image = loadImageFromUrl(url)
-        self.contentImage = prepImage(image)
+        self.contentImage = loadImageFromUrl(url)
 
     def setStyleFromPath(self, path):
         '''
@@ -86,8 +59,7 @@ class Dream():
         :param path: Path to the image file
         '''
         #Load image and convert to RGB. Standard is RBG
-        image = Image.open(path).convert('RGB')
-        self.styleImage = prepImage(image)
+        self.styleImage = Image.open(path).convert('RGB')
 
     def setStyleFromUrl(self, url):
         '''
@@ -95,8 +67,7 @@ class Dream():
 
         :param url: URL to the image
         '''
-        image = loadImageFromUrl(url)
-        self.styleImage = prepImage(image)
+        self.styleImage = loadImageFromUrl(url)
 
     def setParams(self):
         '''
@@ -105,7 +76,7 @@ class Dream():
         '''
         pass #TODO implement me
 
-    def train(self, epochs):
+    def apply(self, epochs):
         '''
         Train the model. Content image, style image and parameters must have been set before.
 
