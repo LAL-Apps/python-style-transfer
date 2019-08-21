9d6db8af6b4f2146560f90bab6b00d317c80bfe4
Lorenz <apps.lal@gmail.com>
2019-08-21 22:30:00 +0200

    Added about file with version number etc.

--- deepdream/__init__.py
+++ deepdream/__init__.py
@@ -1,5 +1,17 @@
 from .dream import Dream
 
+#Import constants
+from deepdream.__about__ import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __summary__,
+    __title__,
+    __uri__,
+    __version__,
+)
+
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
 from logging import NullHandler
