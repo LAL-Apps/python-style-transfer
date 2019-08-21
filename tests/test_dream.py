59d6c2fa6167aed3cb08e31f8c32491583492d4f
Lorenz <apps.lal@gmail.com>
2019-08-21 22:01:36 +0200

    Added clean-up after tests

--- tests/test_dream.py
+++ tests/test_dream.py
@@ -21,15 +21,16 @@ def test_train_with_images_from_file():
     img = dream.apply(1)
     assert img is not None
 
-    dream.writeFinalImage('temp.jpg')
-    #TODO test if file exsits and delete
+    dream.writeFinalImage('deepdream-test.jpg')
+    assert os.path.isfile('deepdream-test.jpg')
+
+    #Delete the file again
+    os.remove('deepdream-test.jpg')
 
 
-
-def test_train_with_images_from_url():
+def test_load_images_from_url():
     dream = deepdream.Dream()
     dream.setContentFromUrl('https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Felis_silvestris_catus_lying_on_rice_straw.jpg/640px-Felis_silvestris_catus_lying_on_rice_straw.jpg')
     dream.setStyleFromUrl('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Tsunami_by_hokusai_19th_century.jpg/640px-Tsunami_by_hokusai_19th_century.jpg')
     assert dream.contentImage is not None
     assert dream.styleImage is not None
-    #TODO
